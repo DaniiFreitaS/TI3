b06182fbdc7d9683ca005ffb926b1edfb1d431e8
DaniiFreitaS <danielfreitaslele14@gmail.com>
2026-05-10 21:57:12 -0300

    att script spawn
    as posições de spawn agora estao em vetor pra colocar mais de uma se necessario

--- Defenda o Castelo/Assets/Scripts/SpawnDefesa.cs
+++ Defenda o Castelo/Assets/Scripts/SpawnDefesa.cs
@@ -4,8 +4,9 @@ public class SpawnDefesa : MonoBehaviour
 {
     public GameObject prefabToSpawn;
 
-    // Esta variável vai guardar o seu objeto vazio (o "null")
-    public Transform pontoDeSpawn;
+    //variavel para o transform do Spawn
+    public Transform[] pontoDeSpawn;
+    //escalando os bonecos em 6 pra ficar grande na tela
     public float novaEscala = 6f;
 
     private void OnMouseDown()
@@ -14,11 +15,12 @@ public class SpawnDefesa : MonoBehaviour
         if (prefabToSpawn != null && pontoDeSpawn != null)
         {
             // Agora instanciamos na posição e rotação do 'pontoDeSpawn' em vez do objeto clicado
-            GameObject novoObjeto = Instantiate(prefabToSpawn, pontoDeSpawn.position, pontoDeSpawn.rotation);
-
-            novoObjeto.transform.localScale = new Vector3(novaEscala, novaEscala, novaEscala);
-
-            Debug.Log("Novo prefab criado na área fixa!");
+            foreach (Transform i in pontoDeSpawn)
+            {
+                GameObject novoObjeto = Instantiate(prefabToSpawn, i.position, i.rotation);
+                novoObjeto.transform.localScale = new Vector3(novaEscala, novaEscala, novaEscala);
+                Debug.Log("Novo prefab criado");
+            }   
         }
         else
         {
